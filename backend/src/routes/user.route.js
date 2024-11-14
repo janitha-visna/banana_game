9ad5addb2b395ea4367c0aafe7f2e447010e79a5
janitha-visna <visnajanitha@gmail.com>
2024-11-14 20:44:03 +0530

    added middleware to submitscore route

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 
 import { submitscore } from "../controllers/score.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/add-score", submitscore);
+router.post("/add-score",verifyToken, submitscore);
 
 export default router;
