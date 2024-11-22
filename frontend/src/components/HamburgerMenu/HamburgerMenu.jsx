92a67a87de18afc1078caf9f88bde174e898b4a7
janitha-visna <visnajanitha@gmail.com>
2024-11-22 21:11:36 +0530

    refactor homepage

---
+++ frontend/src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./HamburgerMenu.css";
+
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="hamburger-menu">
+      <button className="hamburger-icon" onClick={toggleMenu}>
+        ☰
+      </button>
+      {isOpen && (
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- frontend/src/routes/homepage/Homepage.jsx
+++ frontend/src/routes/homepage/Homepage.jsx
@@ -2,14 +2,31 @@ import Leaderboard from "../../components/leaderboard/Leaderbord";
 import styles from "./homepage.module.css";
 import leaderboardData from "../../components/leaderboard/leaderboardData";
 import Login from "../Login/Login";
+import HamburgerMenu from "../../components/HamburgerMenu/HamburgerMenu";
+import { useNavigate } from "react-router-dom";
+import Selectoptions from "../../components/options/Selectoptions";
 
 function Homepage() {
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    navigate(path);
+  };
+
   return (
     <div className={styles.homepage}>
-      <div className={styles.left}>
-        <Login />
+      <div
+        className={styles.box}
+        onClick={() => handleClick("/selectoption")} // Navigate to Component1
+      >
+        Single Player
       </div>
-      <div className={styles.right}></div>
+      <div
+        className={styles.box}
+        onClick={() => handleClick("/component2")} // Navigate to Component2
+      >
+        Multi Player
+      </div>
     </div>
   );
 }
