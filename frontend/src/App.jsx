0fa607407df19637e1e70135decfcca62fdbe1db
janitha-visna <visnajanitha@gmail.com>
2024-12-05 17:51:44 +0530

    refactor protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,27 +2,15 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useAuth } from "./context/AuthContext"; // Import hook
 import Homepage from "./routes/homepage/Homepage";
-import Multiplayer from "./routes/multiplayer/Multiplayer";
 import Leaderboard from "./components/leaderboard/Leaderbord";
 import Game from "./components/singleplayer/Singleplayer";
 import Selectoption from "./components/options/Selectoptions";
-import Records from "./components/Userrecords/UserRecords"
+import Records from "./components/Userrecords/UserRecords";
 import Login from "./routes/Login/Login";
 import Register from "./routes/Register/Register";
-import NotFound from "./routes/Notfound/Notfound";
+import NotFound from "./components/Notfound/Notfound";
 import leaderboardData from "./components/leaderboard/leaderboardData";
 import Playgame from "./routes/Playgame/Playegame";
-
-// Debugging: Log components to ensure they are properly imported
-console.log("Homepage:", Homepage); // Should log the function/component definition
-console.log("Multiplayer:", Multiplayer);
-console.log("Leaderboard:", Leaderboard);
-console.log("Game:", Game);
-console.log("Selectoption:", Selectoption);
-console.log("Records:", Records);
-console.log("Login:", Login);
-console.log("Register:", Register);
-console.log("NotFound:", NotFound);
 
 const App = () => {
   const { RequireAuth } = useAuth(); // Access RequireAuth
@@ -32,19 +20,23 @@ const App = () => {
       path: "/",
       element: <Homepage />, // Public route
     },
-    {
-      path: "/multiplayer",
-      element: <Multiplayer />, // Public route
-    },
+    // Protected Routes
     {
       path: "/leaderboard",
-      element: <Leaderboard data={leaderboardData} />, // Public route
+      element: (
+        <RequireAuth>
+          <Leaderboard data={leaderboardData} />
+        </RequireAuth>
+      ),
     },
     {
-      path:"/play",
-      element: <Playgame/>,
+      path: "/play",
+      element: (
+        <RequireAuth>
+          <Playgame />
+        </RequireAuth>
+      ),
     },
-    // Protected Routes
     {
       path: "/singleplayer",
       element: (
